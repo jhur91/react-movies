6643df12fbacb4a0983f25a793844f9e94f40b6b
Jason Hur <jasonyhur@gmail.com>
2020-07-20 17:26:21 -0700

    fix news deploy

--- src/services/news-api.js
+++ src/services/news-api.js
@@ -1,5 +1,5 @@
 
 export function getAll() {
-  return fetch(`http://newsapi.org/v2/top-headlines?country=us&category=entertainment&apiKey=4be6ad4a89bc47bbad612f671c80e92c`, {mode: 'cors'})
+  return fetch(`https://newsapi.org/v2/top-headlines?country=us&category=entertainment&apiKey=4be6ad4a89bc47bbad612f671c80e92c`, {mode: 'cors'})
       .then(res => res.json())
 }
